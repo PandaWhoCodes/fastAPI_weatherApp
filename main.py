d431992c0cffceb5c3e30c370bf748f528e6eb4d
Thomas Ashish Cherian <ufoundashish@gmail.com>
2022-09-15 17:33:13 +0530

    add css and html

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from msilib.schema import Directory
 import uvicorn
 import fastapi
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+
 api = fastapi.FastAPI()
 
 templates = Jinja2Templates("templates")
+api.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @api.get("/")
-def index():
-    return "Hello Weather App!"
-
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
-    uvicorn.run(api,port=8000,host="0.0.0.0")
+    uvicorn.run(api, port=8000, host="0.0.0.0")
+
